77d74519531e357d9882410fbf654aeb2512c1a2
ChristopherGray <hristo_yovchev01@abv.bg>
2015-02-16 18:18:05 +0200

    Created by Coding Ground Client

---
+++ New Project/main.py
@@ -0,0 +1,22 @@
+import random
+import sys
+import os
+
+print("Hallo World")
+
+name = "Derek"
+print(name)
+
+print("2 * 3 =", 6)
+print("30 / 6 =", 5)
+print("5 * 6 =", 40 - 10)
+
+quote = "I am the best" 
+multi_line_quote = '''Yes you are'''  
+
+print("%s %s %s" % ('Hello', quote , multi_line_quote ))
+
+print("I dont like this", end="")
+print("newline")
+
+print("I am the best\n" * 5 )
