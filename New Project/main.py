5b8def609a84847d6c67e71e86015d7daa808b5a
ChristopherGray <hristo_yovchev01@abv.bg>
2015-02-16 20:20:54 +0200

    Created by Coding Ground Client

---
+++ New Project/main.py
@@ -0,0 +1,16 @@
+import random
+import sys
+import os
+
+age = 30
+
+    
+if((age >= 1) and (age <= 18)):
+    print("You are awsome")
+elif ((age==21) or (age<=65)):
+    print("You are awsome")
+
+elif not(age == 30):
+    print("You are not awsome")
+else:
+    print("You get a party")
